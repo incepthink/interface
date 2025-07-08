c1056662dd9aeb1baca22647b0d59629a784cf47
incepthink <incepthink@gmail.com>
2025-07-08 19:07:28 +0530

    chanegs

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,6 @@ import { Box } from '@mui/material';
 import React, { ReactNode } from 'react';
 import AnalyticsConsent from 'src/components/Analytics/AnalyticsConsent';
 import { FeedbackModal } from 'src/layouts/FeedbackDialog';
-import { FORK_ENABLED } from 'src/utils/marketsAndNetworksConfig';
 
 import { AppFooter } from './AppFooter';
 import { AppHeader } from './AppHeader';
@@ -43,7 +42,7 @@ export function MainLayout({ children }: { children: ReactNode }) {
       </Box>
       <AppFooter />
       <FeedbackModal />
-      {FORK_ENABLED ? null : <AnalyticsConsent />}
+      {true ? null : <AnalyticsConsent />}
     </>
   );
 }
